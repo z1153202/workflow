04c4594f011cc04b84da25fe68c3fe2545e9f223
yasuogu <yasuogu@2dfire.com>
2022-08-01 16:07:57 +0800

    little change

--- genshin/genshin.py
+++ genshin/genshin.py
@@ -2,15 +2,7 @@
 # coding=utf-8
 import datetime
 import requests
-header = {
-    'Host': 'www.zhihu.com',
-    'Accept-Encoding': 'gzip, deflate',
-    'Accept': '*/*',
-    'User-Agent': 'PostmanRuntime/7.17.1',
-    'Cache-Control': 'no-cache',
-    'Connection': 'keep-alive',
-}
-url = 'https://www.zhihu.com/api/v4/members/guodongxiaren?include=follower_count%2cvoteup_count'
+url = 'http://httpbin.org/get'
 resp = requests.get(url)
 data = resp.json()
 print(data)
